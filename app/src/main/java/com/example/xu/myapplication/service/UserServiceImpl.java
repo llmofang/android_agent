f4e23978e37b289689e573d56a5bf6080dd8fe73
xuuj <517534219@qq.com>
2015-02-12 08:26:15 +0800

    xx

--- app/src/main/java/com/example/xu/myapplication/service/UserServiceImpl.java
+++ app/src/main/java/com/example/xu/myapplication/service/UserServiceImpl.java
@@ -1,31 +1,22 @@
 package com.example.xu.myapplication.service;
 
-import android.app.DownloadManager;
+
 import android.util.Log;
 
-import org.apache.http.HttpHost;
+
+import com.llmofang.android.agent.LLMoFangUtil;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.params.ConnRouteParams;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import com.llmofang.android.agent.HttpInstrumentation;
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.OkUrlFactory;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by think on 14-11-10.
@@ -42,25 +33,21 @@ public class UserServiceImpl implements UserService {
 
         //Thread.sleep(3000);
 
-//        HttpClient client = new DefaultHttpClient();
-//        String uri = "http://www.baidu.com";
-//        HttpGet get = new HttpGet(uri);
-//        ResponseHandler<String> responseHandle = new BasicResponseHandler();
-//
-//
-//        String responseBody =(String) client.execute(get,responseHandle);
+        HttpClient client = new DefaultHttpClient();
+        String uri = "http://www.baidu.com";
+        HttpGet get = new HttpGet(uri);
+        ResponseHandler<String> responseHandle = new BasicResponseHandler();
 
 
-      //  Log.i(TAG, responseBody);
+        HttpResponse response = client.execute(get);
+        String responseBody= EntityUtils.toString(response.getEntity());
+
+        Log.i(TAG, responseBody);
 //        URL url = new URL("http://www.baidu.com");
-//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-//        connection.getURL().openConnection();
-//        connection.connect();
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(
-//                connection.getInputStream()));
+//        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
 //
-//
-//        reader.close();
+//       String s= LLMoFangUtil.ConvertToString(connection.getInputStream());
+//        Log.i("[llmofang]",s);
 //        // 断开连接
 //        connection.disconnect();
 
@@ -77,46 +64,46 @@ public class UserServiceImpl implements UserService {
 //        Log.i(TAG, response.body().string());
 
 
-        OkHttpClient client = new OkHttpClient();
-        URL url = new URL("http://www.baidu.com");
-        //Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress("115.238.145.51", 59901));
-        HttpURLConnection connection = new OkUrlFactory(client).open(url);
-        connection.connect();
-        OutputStream out = null;
-        InputStream in = null;
-        try {
-            // Write the request.
-            connection.setRequestMethod("POST");
-            out = connection.getOutputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            System.out.println("=============================");
-            System.out.println("Contents of get request");
-            System.out.println("=============================");
-            String lines;
-            while ((lines = reader.readLine()) != null) {
-                System.out.println(lines);
-                Log.i(TAG, lines);
-            }
-            reader.close();
-            // 断开连接
-            connection.disconnect();
-            System.out.println("=============================");
-            System.out.println("Contents of get request ends");
-            System.out.println("=============================");
-            // out.write(body);
-            out.close();
-
-            // Read the response.
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException("Unexpected HTTP response: "
-                        + connection.getResponseCode() + " " + connection.getResponseMessage());
-            }
-            in = connection.getInputStream();
-        } finally {
-            // Clean up.
-            if (out != null) out.close();
-            if (in != null) in.close();
-        }
+//        OkHttpClient client = new OkHttpClient();
+//        URL url = new URL("http://www.baidu.com");
+//        //Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress("115.238.145.51", 59901));
+//        HttpURLConnection connection = new OkUrlFactory(client).open(url);
+//        connection.connect();
+//        OutputStream out = null;
+//        InputStream in = null;
+//        try {
+//            // Write the request.
+//            connection.setRequestMethod("POST");
+//            out = connection.getOutputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(
+//                    connection.getInputStream()));
+//            System.out.println("=============================");
+//            System.out.println("Contents of get request");
+//            System.out.println("=============================");
+//            String lines;
+//            while ((lines = reader.readLine()) != null) {
+//                System.out.println(lines);
+//                Log.i(TAG, lines);
+//            }
+//            reader.close();
+//            // 断开连接
+//            connection.disconnect();
+//            System.out.println("=============================");
+//            System.out.println("Contents of get request ends");
+//            System.out.println("=============================");
+//            // out.write(body);
+//            out.close();
+//
+//            // Read the response.
+//            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//                throw new IOException("Unexpected HTTP response: "
+//                        + connection.getResponseCode() + " " + connection.getResponseMessage());
+//            }
+//            in = connection.getInputStream();
+//        } finally {
+//            // Clean up.
+//            if (out != null) out.close();
+//            if (in != null) in.close();
+//        }
     }
 }
